d604508e538861268abab9b674ca5515c262d8a6
shcrodger <30347969+shcrodger@users.noreply.github.com>
2017-07-30 12:15:41 -0400

    Add files via upload

---
+++ Lab5Ex8.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+
+char* scanline(char *dest, int dest_len);
+
+char name[80];
+char lastName[15];
+char firstName[10];
+char middleName[2];
+int filler;
+
+
+int i = 0;
+int p = 0;
+
+int main()
+{
+	printf("Please enter a name with the last name first (length 15), with a comman and a space after it, then the first name (length 10) with a full stop after it and the middle initial (length 1) with a full stop after it. The total amount of characters must not go above 80. ");
+	scanline(name, 80);
+	while(name[i] != ','){
+		lastName[p] = name[i];
+		i++;
+		p++;
+	}
+	i = i + 2;
+	p = 0;
+	while(name[i] != '.'){
+		firstName[p] = name[i];
+		i++;
+		p++;
+	}
+	i = i + 2;
+	p = 0;
+	while(name[i] != '.'){
+		middleName[p] = name[i];
+		i++;
+		p++;
+	}
+	for(i = 0; i < strlen(lastName); i++){
+		printf("%c", lastName[i]);
+	}
+	if(strlen(lastName) != 15){
+		filler = 15 - strlen(lastName);
+		for(i = 0; i <= filler; i++){
+			printf(" ");
+		}
+	}
+	printf("     ");
+	for(i = 0; i < strlen(firstName); i++){
+		printf("%c", firstName[i]);
+	if(strlen(firstName) != 10);
+		filler = 10 - strlen(firstName);
+		for(i = 0; i <= filler; i++){
+			printf(" ");
+		}
+	}
+	printf("     ");
+	printf("%c \n", middleName[0]);
+return 0;
+}
+
+char * //This code is 'scanline.c' from the course page website
+scanline(char *dest, /* output - destination string */
+         int dest_len) /* input - space available in dest */
+{
+  int i, ch;
+
+  /* Gets next line one character at a time. */
+  i = 0;
+  for (ch = getchar();
+       ch != '\n' && ch != EOF && i < dest_len - 1;
+       ch = getchar())
+    dest[i++] = ch;
+  dest[i] = '\0';
+}
